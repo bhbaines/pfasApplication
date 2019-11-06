1ff045a7c62163d7afb2072c265af65063453d11
bhbaines <blakebaines@gmail.com>
2019-11-05 20:34:36 -0500

    changed inputs to tab delimted to deal with commas in chemical name descriptions

--- createChem_con.py
+++ createChem_con.py
@@ -4,7 +4,7 @@ import csv
 
 
 gpsFile = open(sys.argv[1], 'r')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\gps_sites_v6.txt'  
-chem_tempFile = open(sys.argv[2], 'r')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\chem_con_template.csv'
+chem_tempFile = open(sys.argv[2], 'r')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\chem_con_template.txt'
 chem_conFile = open(sys.argv[3], 'w')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\chem_con.csv'
 chem_conWrite = csv.writer(chem_conFile, delimiter=',', lineterminator='\n')
 epa_IDs = []
@@ -22,11 +22,10 @@ rowRestart = 46
 
 for id in epa_IDs:
     for row in chemRead:
-        cells = row.split(',')
+        cells = row.split('\t')
         cellsCleaned = [cell.strip() for cell in cells]
-        cellsdeepCleaned = [cell.strip('"') for cell in cellsCleaned]
-        cellsdeepCleaned.insert(0,id)
-        chem_conWrite.writerow(cellsdeepCleaned)
+        cellsCleaned.insert(0,id)
+        chem_conWrite.writerow(cellsCleaned)
     
 
 
