cd8ee086b7d0ade41534f7c51d449a19e60ae3aa
bhbaines <blakebaines@gmail.com>
2019-11-05 19:20:40 -0500

    written to programatically generate data for chem_con table given the gps sites tab delimited file and relavent rows from HTML chemical concentrations report

---
+++ createChem_con.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import csv
+
+
+gpsFile = open(sys.argv[1], 'r')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\gps_sites_v6.txt'  
+chem_tempFile = open(sys.argv[2], 'r')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\chem_con_template.csv'
+chem_conFile = open(sys.argv[3], 'w')  #r'C:\NCSU_MGIST_Portofolio_Archive\GIS590\Project\Data\chem_con.csv'
+chem_conWrite = csv.writer(chem_conFile, delimiter=',', lineterminator='\n')
+epa_IDs = []
+
+
+gpsRead = gpsFile.readlines()[1:]
+chemRead = chem_tempFile.readlines()
+for row in gpsRead:
+    cells = row.split('\t')
+    epaID = cells[10]
+    epa_IDs.append(epaID)
+
+rowIncrement = 0
+rowRestart = 46
+
+for id in epa_IDs:
+    for row in chemRead:
+        cells = row.split(',')
+        cellsCleaned = [cell.strip() for cell in cells]
+        cellsdeepCleaned = [cell.strip('"') for cell in cellsCleaned]
+        cellsdeepCleaned.insert(0,id)
+        chem_conWrite.writerow(cellsdeepCleaned)
+    
+
+
+
+gpsFile.close()
+chem_tempFile.close()
+chem_conFile.close()
