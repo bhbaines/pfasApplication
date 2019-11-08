2f961b4d6ba20f1c9b51a1896d318c6b8e98d020
Vaclav Petras <wenzeslaus@gmail.com>
2019-11-08 11:52:06 -0500

    Use g.list to get input, parametrize, fix syntax

--- batchRwatershed.py
+++ batchRwatershed.py
@@ -1,17 +1,19 @@
 import sys
-import grass.script as grass
+import grass.script as gs
 import os
 import re
 
-vectDir = sys.argv[1] #Directory with contributing area vectors
-vectFiles = os.listdir(vectDir)
-rExternal = sys.argv[2] #name of external raster
+# all: ^ContArea_SW_[0-9]+_vect$
 
-pattern = re.compile('Cont.') #Could make text pattern an input
-contAreas = [filename for filename in vectFiles if re.match(pattern, filename)]
+if len(sys.argv) != 1 and sys.argv[1]:
+    pattern = sys.argv[1]
+else:
+    sys.exit("Usage: script.py pattern (e.g., ^ContArea_SW_[0-9]+_vect$)")
+
+contAreas = gs.read_command("g.list", type="vector", pattern="^ContArea_SW_[0-9]+_vect$", flags="e", mapset=".").splitlines()
 
 for area in contAreas:
-    grass.run_command(g.region vector=area)
-    grass.run_command(r.watershed elevation=rExternal, accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir')
+    gs.run_command("g.region", vector=area)
+    gs.run_command("r.watershed", elevation="ned_19arc", accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir')
 
 
