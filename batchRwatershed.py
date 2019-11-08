de5d0aa2cbc870a100ce24cae566204d47f8c556
bhbaines <blakebaines@gmail.com>
2019-11-08 09:44:29 -0500

    added comments to describe variables

--- batchRwatershed.py
+++ batchRwatershed.py
@@ -1,14 +1,17 @@
 import sys
-#import grass.script as grass
+import grass.script as grass
 import os
 import re
 
-vectDir = sys.argv[1] #C:/NCSU_MGIST_Portofolio_Archive/GIS582/GRASSData/nc_spm_08_grass7/pfas_02/vector
+vectDir = sys.argv[1] #Directory with contributing area vectors
 vectFiles = os.listdir(vectDir)
+rExternal = sys.argv[2] #name of external raster
 
-pattern = re.compile('Cont.')
+pattern = re.compile('Cont.') #Could make text pattern an input
 contAreas = [filename for filename in vectFiles if re.match(pattern, filename)]
 
 for area in contAreas:
     grass.run_command(g.region vector=area)
-    grass.run_command(r.watershed )
+    grass.run_command(r.watershed elevation=rExternal, accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir')
+
+
