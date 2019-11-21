5e7ffe2235ebe7c87b0d8a89f668c49ab32410cd
Vaclav Petras <wenzeslaus@gmail.com>
2019-11-21 17:22:22 -0500

    Parameterize elevation, use it for g.region
    Also tell where the error was, add exclude pattern, look into all accessible mapsets
    (so the responsibility is on the user to make only the right ones accessible),
    hardcoded high limited memory and overwrite.

--- batchRwatershed.py
+++ batchRwatershed.py
@@ -3,17 +3,25 @@ import grass.script as gs
 import os
 import re
 
-# all: ^ContArea_SW_[0-9]+_vect$
-
-if len(sys.argv) != 1 and sys.argv[1]:
-    pattern = sys.argv[1]
+if len(sys.argv) in (3, 4) and sys.argv[1] and sys.argv[2]:
+    elevation = sys.argv[1]
+    pattern = sys.argv[2]
+    if len(sys.argv) == 4:
+        exclude = sys.argv[3]
+    else:
+        exclude = None
 else:
-    sys.exit("Usage: script.py pattern (e.g., ^ContArea_SW_[0-9]+_vect$)")
+    sys.exit("Usage: script.py elevation pattern [exclude]\nExample: %s dem '^ContArea_SW_[0-9]+_vect$'" % sys.argv[0])
 
-contAreas = gs.read_command("g.list", type="vector", pattern=pattern, flags="e", mapset=".").splitlines()
+contAreas = gs.read_command("g.list", type="vector", pattern=pattern, exclude=exclude, flags="e").splitlines()
+
+gs.run_command("g.region", raster=elevation)
 
 for area in contAreas:
-    gs.run_command("g.region", vector=area)
-    gs.run_command("r.watershed", elevation="ned_19arc", accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir')
+    gs.run_command("g.region", vector=area, align=elevation)
 
-
+    try:
+        gs.run_command("r.watershed", elevation=elevation, accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir', memory=131072, flags="m", overwrite=True)
+    except gs.CalledModuleError:
+        print("Failed computing:", area)
+        raise
