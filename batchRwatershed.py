f44d1acdcde7c30612284da59a1f47e061ad3a5a
Vaclav Petras <wenzeslaus@gmail.com>
2019-11-22 09:40:52 -0500

    Parametrize memory option

--- batchRwatershed.py
+++ batchRwatershed.py
@@ -3,15 +3,16 @@ import grass.script as gs
 import os
 import re
 
-if len(sys.argv) in (3, 4) and sys.argv[1] and sys.argv[2]:
-    elevation = sys.argv[1]
-    pattern = sys.argv[2]
-    if len(sys.argv) == 4:
-        exclude = sys.argv[3]
+if len(sys.argv) in (4, 5) and sys.argv[1] and sys.argv[2] and sys.argv[3]:
+    memory = sys.argv[1]
+    elevation = sys.argv[2]
+    pattern = sys.argv[3]
+    if len(sys.argv) == 5:
+        exclude = sys.argv[4]
     else:
         exclude = None
 else:
-    sys.exit("Usage: script.py elevation pattern [exclude]\nExample: %s dem '^ContArea_SW_[0-9]+_vect$'" % sys.argv[0])
+    sys.exit("Usage: script.py memory elevation pattern [exclude]\nExample: %s dem '^ContArea_SW_[0-9]+_vect$'" % sys.argv[0])
 
 contAreas = gs.read_command("g.list", type="vector", pattern=pattern, exclude=exclude, flags="e").splitlines()
 
@@ -19,11 +20,19 @@ gs.use_temp_region()
 
 gs.run_command("g.region", raster=elevation)
 
+if memory == "all":
+    memory = None
+    flags = None
+else:
+    flags = "m"
+
+print("Processing {} raster maps...".format(len(contAreas)))
+
 for area in contAreas:
     gs.run_command("g.region", vector=area, align=elevation)
 
     try:
-        gs.run_command("r.watershed", elevation=elevation, accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir', memory=131072, flags="m", overwrite=True)
+        gs.run_command("r.watershed", elevation=elevation, accumulation=area[:-4] + 'accum', drainage=area[:-4] + 'draindir', memory=memory, flags=flags, overwrite=True)
     except gs.CalledModuleError:
         print("Failed computing:", area)
         raise
